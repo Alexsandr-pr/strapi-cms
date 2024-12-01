6f3aedfd9ce6a15a3014fe98d360112741983c1d
Alex Pererodov <pererodovsasha2362000@gmail.com>
2024-12-01 15:36:18 +0200

    add button component

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,22 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface LayoutButton extends Struct.ComponentSchema {
+  collectionName: 'components_layout_buttons';
+  info: {
+    displayName: 'Button';
+  };
+  attributes: {
+    buttonsColorBlue: Schema.Attribute.Boolean &
+      Schema.Attribute.DefaultTo<false>;
+    targetBlank: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<true>;
+    url: Schema.Attribute.Text & Schema.Attribute.Required;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'layout.button': LayoutButton;
+    }
+  }
+}
